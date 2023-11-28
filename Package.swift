1d58839d38b613473e0f17a48685ae4b02f9d2e2
shubhamahire07 <shubham.ahire@tappp.com>
2023-11-28 13:05:38 +0530

    Adding resources

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Tappp_Library"),
+            name: "Tappp_Library",
+            resources: [
+                .copy("Tappp_Library/dist")
+            ]
+        ),
         .testTarget(
             name: "Tappp_LibraryTests",
             dependencies: ["Tappp_Library"]),
